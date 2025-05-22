14b097cd8f10583f4ed76071585958d697f3b87c
dilipv <dilipv@nbnco.com.au>
2025-05-22 21:06:21 +1000

    Initial Commit

---
+++ server-java/src/main/java/au/com/dv/coreconcepts/serverjava/ServerJavaApplication.java
@@ -0,0 +1,13 @@
+package au.com.dv.coreconcepts.serverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerJavaApplication.class, args);
+    }
+
+}
---
+++ server-java/src/test/java/au/com/dv/coreconcepts/serverjava/ServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.dv.coreconcepts.serverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
